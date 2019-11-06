eba3b3483bb6b64c94f40cb7c027777222880e51
MohsinKhanzada97 <MohsinKhanzada97@users.noreply.github.com>
2019-11-06 21:03:49 +0500

    programming fundamental exponent

---
+++ programming fundamental/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace programming_fundamental
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = 0;
+            int exponent = 0;
+            int sum = 1;
+            Console.WriteLine("Enter Number:");
+            num = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter Exponent:");
+            exponent = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 0; i < exponent; i++)
+            {
+                sum = sum * num;
+            }
+
+            Console.Write("The Exponent Of " + num + " to " + exponent + " is:" + sum);
+            Console.ReadKey();
+        }
+    }
+}
